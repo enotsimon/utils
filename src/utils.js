05dcdedd299b1b7be9254fa0795e6cac63acaeb3
enot simon <sy.enot@gmail.com>
2022-07-22 14:07:12 +0300

    add indexById

--- src/utils.js
+++ src/utils.js
@@ -41,6 +41,10 @@ export const execInCycleWithDelay = (
   setTimeout(() => { execInCycleWithDelay(index + 1, limit, delay, func, final_func) }, delay)
 }
 
+type FAV = <V>(xs: $ReadOnlyArray<V>) => { [string]: V }
+// $FlowIgnore
+export const indexById = (): FAV => R.indexBy(R.prop('id'))
+
 export const randElement = <T>(arr: Array<T>): T => {
   if (arr.length === 0) {
     throw new Error('randElement input array cannot be empty')
